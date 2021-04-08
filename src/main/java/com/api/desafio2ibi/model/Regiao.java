1313e364224742bfa860aebb5b5cced6a0fa68f7
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-08 23:26:10 +0200

    relacionamento regiao ubregiao

--- src/main/java/com/api/desafio2ibi/model/Regiao.java
+++ src/main/java/com/api/desafio2ibi/model/Regiao.java
@@ -1,9 +1,12 @@
 package com.api.desafio2ibi.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Regiao {
@@ -11,6 +14,9 @@ public class Regiao {
 	/* ID -> dentificador – gerado automaticamente/*/
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	@OneToMany
+	private List<SubRegiao> subRegioao;
+	
 	private String nome;
 	public String getNome() {
 		return nome;
