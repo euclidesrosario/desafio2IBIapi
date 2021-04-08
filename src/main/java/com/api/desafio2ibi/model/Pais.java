e4577f9c03dc906346e9e25daa41297da4ae3e6e
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-08 23:23:09 +0200

    Tabela pais

--- src/main/java/com/api/desafio2ibi/model/Pais.java
+++ src/main/java/com/api/desafio2ibi/model/Pais.java
@@ -15,6 +15,21 @@ public class Pais {
 	
 	private String nome;
 	private String capital;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public long getId() {
+		return id;
+	}
 	
 	
 	
