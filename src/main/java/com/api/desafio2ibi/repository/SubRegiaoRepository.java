c383b53f165b85dedcb2a5e918ab116db8ec1d4f
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-11 22:08:57 +0200

    criando o repositorio para a injecao da regiao

---
+++ src/main/java/com/api/desafio2ibi/repository/SubRegiaoRepository.java
@@ -0,0 +1,9 @@
+package com.api.desafio2ibi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.desafio2ibi.model.SubRegiao;
+
+public interface SubRegiaoRepository extends JpaRepository<SubRegiao, Long>{
+
+}
