7bbc060c5cd442139cdc15c68c0a01be6be018e3
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-13 08:15:23 +0200

    injectando regiao no resource

--- src/main/java/com/api/desafio2ibi/resource/RegiaoResource.java
+++ src/main/java/com/api/desafio2ibi/resource/RegiaoResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,17 +22,25 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.api.desafio2ibi.model.Regiao;
 import com.api.desafio2ibi.repository.RegiaoRepository;
+import com.api.desafio2ibi.services.RegiaoServices;
 
 @RestController
 @RequestMapping("/regioes")
 public class RegiaoResource {
-	@Autowired
-	private RegiaoRepository regiaoRepository;
+	
+	@Autowired RegiaoServices regiaoService;
+	@Autowired private RegiaoRepository regiaoRepository;
 	
 	
 	@GetMapping
 	public List<Regiao> listar(){
 		return (List<Regiao>) regiaoRepository.findAll();	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Regiao> pesquisar(@PathVariable("id") long id){
+		Regiao regiao = regiaoRepository.findById(id);
+		return regiao !=null? ResponseEntity.ok(regiao) : ResponseEntity.notFound().build() ;
 	}
 	
 	@PostMapping
@@ -44,16 +53,23 @@ public class RegiaoResource {
 		return ResponseEntity.created(uri).body(regiaoSalva);
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<Regiao> pesquisar(@PathVariable("id") long id){
-		Regiao regiao = regiaoRepository.findById(id);
-		return regiao !=null? ResponseEntity.ok(regiao) : ResponseEntity.notFound().build() ;
+	
+	
+	@PutMapping()
+	public ResponseEntity<Regiao> atualizar(@PathVariable("id") long id, @Valid @RequestBody Regiao regiao){
+		Regiao RegiaoSalva = regiaoService.atualizar(id, regiao);
+		return ResponseEntity.ok(RegiaoSalva);
 	}
+	
+	
+	
+	
 	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover( @PathVariable Long id) {
 		regiaoRepository.deleteById(id);
+		
 		
 	}
 
