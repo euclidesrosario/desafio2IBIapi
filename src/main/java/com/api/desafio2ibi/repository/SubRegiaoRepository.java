24495f54b728e88daa844070d06718cf2abf1d4c
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-04-17 14:18:25 +0200

    criando o metodo que retorna a subregiao pelo nome

--- src/main/java/com/api/desafio2ibi/repository/SubRegiaoRepository.java
+++ src/main/java/com/api/desafio2ibi/repository/SubRegiaoRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.desafio2ibi.model.SubRegiao;
 
 public interface SubRegiaoRepository extends JpaRepository<SubRegiao, Long>{
+	SubRegiao findById(long id);
 
+	SubRegiao findByNome(String nome);
+
+	
 }
